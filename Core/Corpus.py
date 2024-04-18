3742df347779b999f2c52c9b5d796ae2d7831159
wimukthi <9wimu9@gmail.com>
2024-04-18 19:45:49 +0100

    model classes for corpuses and definitions tables

---
+++ Core/Corpus.py
@@ -0,0 +1,37 @@
+import mysql.connector
+
+from Core.MySqlConnector import MySQLConnector
+
+
+class Corpus:
+    db_connector = None
+
+    def __init__(self):
+        self.db_connector = MySQLConnector()
+
+    def list_to_corpus(self, word_list):
+        for word in word_list:
+            print(self.save(word))
+
+    def save(self, word):
+        query = "INSERT INTO `corpuses` (`word`, `created_at`, `updated_at`) VALUES ('" + word + "', now(), now())"
+        try:
+            self.db_connector.cursor.execute(query)
+            self.db_connector.connection.commit()
+            last_id = self.db_connector.cursor.lastrowid
+            self.db_connector.cursor.execute("SELECT * FROM corpuses WHERE id=" + str(last_id))
+            rows = self.db_connector.cursor.fetchall()
+            return rows[0]
+
+        except mysql.connector.IntegrityError:
+            return None
+
+    def delete(self, id):
+        query = "DELETE FROM corpuses WHERE id=" + str(id)
+        self.db_connector.cursor.execute(query)
+        self.db_connector.connection.commit()
+
+    def find_by_word(self, word):
+        self.db_connector.cursor.execute("SELECT * FROM corpuses WHERE word=" + word)
+        return self.db_connector.cursor.fetchall()
+
---
+++ Core/Definition.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+from Core.MySqlConnector import MySQLConnector
+
+word_class_enum = dict(NOUN=1, VERB=2, ADJECTIVE=3, ADVERB=4, PREPOSITION=5, DETERMINER=6, PRONOUN=7, CONJUNCTION=8)
+
+
+class Definition:
+    db_connector = None
+
+    def __init__(self):
+        self.db_connector = MySQLConnector()
+
+    def save(self, corpus_id, word_class, definition):
+        definition = definition.replace("'", "\\'")
+        word_class_id = word_class_enum[word_class.upper()]
+        query = "INSERT INTO `definitions` (`definition`, `corpus_id`, `word_class`, `created_at`, `updated_at`) VALUES ('" + definition + "', " + str(
+            corpus_id) + ", " + str(word_class_id) + ", now(), now())"
+        try:
+            print(query)
+            self.db_connector.cursor.execute(query)
+            self.db_connector.connection.commit()
+            last_id = self.db_connector.cursor.lastrowid
+            self.db_connector.cursor.execute("SELECT * FROM definitions WHERE id=" + str(last_id))
+            rows = self.db_connector.cursor.fetchall()
+            return rows[0]
+
+        except mysql.connector.IntegrityError:
+            return None
