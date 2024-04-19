bbe90f3ae4a3eeb54b94e7ce3eeffa11380975f4
wimukthi <9wimu9@gmail.com>
2024-04-19 18:00:52 +0100

    Capture JSONDECODEERROR

--- Core/Definitions/FreeDictionaryAPI.py
+++ Core/Definitions/FreeDictionaryAPI.py
@@ -12,19 +12,23 @@ class FreeDictionaryAPI:
         self.log = Log()
 
     def get_definition(self, word):
-        response = requests.get(URL + word)
-        if response.status_code != 200:
-            self.log.logger.critical(json.dumps(response.json()))
+        try:
+            response = requests.get(URL + word)
+            if response.status_code != 200:
+                self.log.logger.critical(json.dumps(response.json()))
+                return None
+            contents = response.json()
+            meanings = []
+            for content in contents:
+                for meaning in content['meanings']:
+                    word_class = meaning['partOfSpeech']
+                    for definition in meaning['definitions']:
+                        meanings.append({
+                            'word_class': word_class,
+                            'definition': definition['definition']
+                        })
+            return meanings
+        except requests.exceptions.JSONDecodeError as e:
+            self.log.logger.critical(str(e))
             return None
-        contents = response.json()
-        meanings = []
-        for content in contents:
-            for meaning in content['meanings']:
-                word_class = meaning['partOfSpeech']
-                for definition in meaning['definitions']:
-                    meanings.append({
-                        'word_class': word_class,
-                        'definition': definition['definition']
-                    })
 
-        return meanings
