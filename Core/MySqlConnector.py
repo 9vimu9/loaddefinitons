b7ab220677135e277faa772bd635a66eaf1be568
wimukthi <9wimu9@gmail.com>
2024-05-28 14:32:57 +0100

    implement env in MySqlConnector.py

--- Core/MySqlConnector.py
+++ Core/MySqlConnector.py
@@ -1,8 +1,13 @@
-PORT = 33062
-HOST = '0.0.0.0'
-USER = 'root'
-PASSWORD = 'password'
-DATABASE = 'enhancer_db'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PORT = os.getenv('DB_PORT')
+HOST = os.getenv('DB_HOST')
+USER = os.getenv('DB_USER')
+PASSWORD = os.getenv('DB_PASSWORD')
+DATABASE = os.getenv('DB_DATABASE')
 
 import mysql.connector
 
